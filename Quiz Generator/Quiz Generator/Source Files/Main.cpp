eea7f9e0cb5df6346cbe60f5c972cfd32bf4ffa6
A-VOID-A-VOID-A-VOID <34438014+A-VOID-A-VOID-A-VOID@users.noreply.github.com>
2017-12-26 15:50:15 +1100

    Started creating quiz_question class

--- Quiz Generator/Quiz Generator/Source Files/Main.cpp
+++ Quiz Generator/Quiz Generator/Source Files/Main.cpp
@@ -1,14 +1,16 @@
 //////////////////////////////////////////////////
 // Author/s:			Chris Murphy
 // Date created:		21/12/17
-// Date last edited:	21/12/17
+// Date last edited:	26/12/17
 //////////////////////////////////////////////////
 #include <iostream>
 
+// The entry point of the program.
 int main()
 {
 
 
+	// Pauses the program and waits for the user to press a key.
 	system("pause");
 
 	return 0;
